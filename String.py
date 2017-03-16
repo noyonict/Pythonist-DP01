97cf3117161e8be32ab5c137cb7a76d44a2e62dd
Noyon <noyonmassive@gmail.com>
2017-03-17 00:04:25 +0600

    Create String.py
    All about String, Slicing, indexing, Concatenating etc. 
    Length of String, Adding two string, converting string, int, float etc

---
+++ String.py
@@ -0,0 +1,27 @@
+'''
+a = "python class 4."
+b = "pycharm & pythonist BP01."
+print(a[-2])
+print(b[10:25]+a[6:15])
+print(b[10:]+a[6:])
+print(b[10:],a[7:])
+'''
+
+
+'''a = 4
+b = 'pycharm & pythonist BP01 team'
+c = 62.2
+print(b[0:7],str(c))
+print(b[10:24],'Class',str(a), b[25:])
+
+print(b[:7],c)
+print(b[10:24],'Class',a, b[25:])
+
+print (len(b))
+'''
+
+'''
+b = 'pycharm & pythonist BP01 team'
+print (b.find('pycharm'))
+print (b.find('BP01'))
+'''
