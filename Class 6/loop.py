21faf9ac6d3bfe7f90c2118208902402a7c26f37
Noyon <noyonmassive@gmail.com>
2017-03-23 00:39:47 +0600

    Create loop.py
    for loop, while loop in python
    binary, octal, hexadecimal number

---
+++ Class 6/loop.py
@@ -0,0 +1,53 @@
+#Loop - For loop,  While loop
+
+'''
+for i in range(10):
+    print(i)
+'''
+'''
+i = 0
+while i<=10:
+    print(i)
+    i = i+1
+'''
+
+'''
+l = range(10)
+print(l)
+print(type(l))'''
+
+'''
+for i in [2,4,5,100]:
+    print(i)'''
+
+'''
+for i in range(20,10,-1):
+    print(i)
+
+for i in range(20,10,-2):
+    print(i)
+
+for i in ['a','b','c','d']:
+    print(i)
+
+for i in ['a','b','c','d']:
+    print(ord(i))
+
+print(bin(15))
+
+a = 0b1111
+print(a)
+
+b = 0xa
+print(b)
+
+print(oct(10))
+
+print(hex(15))
+'''
+
+
+#for i in range(100):
+ #   print(ord(str(i)))
+
+'''
