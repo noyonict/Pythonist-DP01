11837757ea40b6794753bf123378e8b9c5a6d301
Noyon <noyonmassive@gmail.com>
2017-03-24 00:31:16 +0600

    Create loop3.py
    loop example

---
+++ Class 7/loop3.py
@@ -0,0 +1,49 @@
+
+'''for line in range(3):
+    for i in range(5):
+        print(1+i,end='  ')
+    print()
+'''
+
+'''a =1
+for i in range(3):
+    i = 0
+    while i<=4:
+        print(a, end='  ')
+        i = i+1
+        a = a+1
+    print()
+'''
+
+
+'''for i in range(1,16):
+    print(i,end='\t')
+    if i%5 == 0:
+        print()
+'''
+
+
+
+#Print *
+
+'''for line in range(3):
+    for i in range(5):
+        print('*',end='  ')
+    print()
+'''
+
+
+'''for line in range(5):
+    for i in range(5):
+        print('*',end='  ')
+    print()
+     '''
+
+
+'''for line in range(5):
+    i = 0
+    for i in range (line):
+        print('*', end=' ')
+        i = i+1
+    print()
+'''
