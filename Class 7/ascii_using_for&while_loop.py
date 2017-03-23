a8a98cbf2b622c886c718899ddbd0191444b1d61
Noyon <noyonmassive@gmail.com>
2017-03-24 00:26:11 +0600

    Create ascii_using_for&while_loop.py
    Int to ascii value using for and while loop in python

---
+++ Class 7/ascii_using_for&while_loop.py
@@ -0,0 +1,22 @@
+'''for i in range(10):
+        print(chr(i))'''
+
+
+'''for i in range(100):
+    print(i,'-',chr(i))'''
+
+
+'''for i in range(33,1000):
+    print(i,chr(i))'''
+
+'''i = 0
+while i<=10:
+    print(chr(i))
+    i = i +1
+    '''
+
+'''i = 0
+while i<=150:
+    print(i,'-',chr(i))
+    i = i+ 1
+'''
