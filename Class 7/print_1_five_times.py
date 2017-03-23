dd689f1ba911871687858ad2ea2d98c046309bf3
Noyon <noyonmassive@gmail.com>
2017-03-24 00:29:53 +0600

    Create print_1_five_times.py
    while loop and for loop example using python3.6

---
+++ Class 7/print_1_five_times.py
@@ -0,0 +1,47 @@
+'''i = 0
+while i <=13:
+    print('1  ',end='')
+    i = i+3
+'''
+
+
+'''i = 0
+while i <=4:
+    print('1  ',end='')
+    i = i+1
+'''
+
+'''i = 0
+while i <=4:
+    print(1,end='  ')
+    i = i+1
+'''
+
+'''for i in range(5):
+    print('1  ',end='')
+'''
+
+'''i = 0
+while i <=4:
+    print(1+i,end='  ')
+    i = i+1
+'''
+
+'''for i in range(1,6):
+    print(i,end='  ')
+'''
+
+'''for i in range(5):
+    print(1+i,end='  ')
+'''
+
+
+'''line = 1
+while line<=3:
+    line = line + 1
+    i = 0
+    while i <=4:
+        print(1+i,end='  ')
+        i = i+1
+    print()
+'''
