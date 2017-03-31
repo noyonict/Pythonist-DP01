2515205e92e122aad4410c651f00084b8bca8ae8
Noyon <noyonmassive@gmail.com>
2017-03-31 18:40:45 +0600

    Create adding_two_number.py
    sum function of python programming

---
+++ Class 8/adding_two_number.py
@@ -0,0 +1,40 @@
+'''
+def add(num1, num2):
+    print(num1+num2)
+add(23,32)'''
+
+
+'''
+def add(num1, num2):
+    print(num1+num2)
+a = int(input('Enter first number: '))
+b = int(input('Enter Second number: '))
+add(a,b)'''
+
+'''
+def sum(a,b):
+    return (a+b)
+x = sum(10,20)
+print(x)'''
+
+'''
+def add(num1, num2):
+    return(num1+num2)
+def mul(num3, num4):
+    return(num3-num4)
+def sub(num5, num6):
+    return(num5*num6)
+def div(num7, num8):
+    return(num7/num8)
+a = int(input('Enter first number: '))
+b = int(input('Enter Second number: '))
+
+x= add(a,b)
+y = mul(a,b)
+z = sub(a,b)
+zz = div(a,b)
+    print(a,'+',b,'=',x)
+    print(a,'-',b,'=',y)
+    print(a,'*',b,'=',z)
+    print(a,'/',b,'=',zz)
+'''
