84a49696adb56678ee7a6e8be630832ed3091e63
Noyon <noyonmassive@gmail.com>
2017-03-31 18:38:42 +0600

    Create function.py
    Function, python function, using and calling function in python

---
+++ Class 8/function.py
@@ -0,0 +1,28 @@
+#Function, Function, Function
+'''
+syntex
+dif function_name():
+    --------
+    --------
+    --------'''
+
+
+'''
+def function_name():
+    print("Md Noyon")
+    print("Denary Computing Limited")
+
+function_name()'''
+
+'''
+d = 5
+def function_name(num):
+    print("Md Noyon")
+    print("Denary Computing Academy")
+    print(num)
+
+function_name('Noyon')
+function_name(923)
+function_name(True)
+function_name(14.5)
+'''
