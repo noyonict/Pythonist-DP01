14420971f3dbce76b412bef95474a56617522d4a
Noyon <noyonmassive@gmail.com>
2017-04-07 00:36:56 +0600

    Create file.py
    Copy and read and write to file from file in python 3

---
+++ Class 11/file.py
@@ -0,0 +1,9 @@
+
+f = open('E:\\Denary Computing Limited\\All Denary Logo\\4.jpg', 'rb')
+prime = f.read()
+f.close()
+
+f = open('C:\\Users\\noyon\\Desktop\\denary.noyon', 'wb')
+f.write(prime)
+f.close()
+
